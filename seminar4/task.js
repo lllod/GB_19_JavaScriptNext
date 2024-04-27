a1b4a0aedc6688d11b30b9d45c4e2f3b2803fc4c
lllod <ygo.admin@gmail.com>
2024-04-27 14:33:04 +0400

    added seminar4

---
+++ seminar4/task.js
@@ -0,0 +1,51 @@
+"use strict";
+// 1. При изменении значения в input с id="from", значение содержащееся в нем должно моментально отображаться в span.
+// То есть при печати в input'е тег span также должен меняться.
+const getInputFrom = document.querySelector('input#from');
+const getSpan = document.querySelector('span');
+getInputFrom.addEventListener('input', function () {
+    getSpan.innerHTML = getInputFrom.value;
+});
+
+// 2. При клике на кнопку с классом messageBtn необходимо элементу с классом message:
+// - добавить два класса: animate_animated и animate_fadeInLeftBig
+// - поставить данному элементу стиль visibility в значение 'visible'.
+const getMessageBtn = document.querySelector('button.messageBtn');
+const getDivMessage = document.querySelector('div.message');
+getMessageBtn.addEventListener('click', function () {
+    getDivMessage.classList.add('animate_animated', 'animate_fadeInLeftBig');
+    getDivMessage.style.visibility = 'visible';
+})
+
+// 3. Необходимо при отправке формы проверить, заполнены ли все поля в этой форме. Если какое-либо поле не заполнено,
+// форма не должна отправляться, также должны быть подсвечены незаполненные поля (необходимо поставить класс error
+// незаполненным полям). Как только пользователь начинает заполнять какое-либо поле, необходимо, при вводе в данное поле,
+// произвести проверку:
+// - Если поле пустое, необходимо данное поле подсветить (поставить класс error данному полю).
+// - Если поле было чем-либо заполнено, подсветку (класс error) необходимо убрать.
+const getForm = document.querySelector('form');
+const getFormInputs = getForm.querySelectorAll('input.form-control, select.form-control');
+getFormInputs.forEach(function (i) {
+    i.addEventListener('input', function () {
+        i.value.trim() === '' ? i.classList.add('error') : i.classList.remove('error');
+    });
+    i.addEventListener('focus', function () {
+        if (i.value.trim() === '') {
+            i.classList.add('error');
+        }
+    });
+});
+getForm.addEventListener('submit', function (e) {
+    let setError = false;
+    getFormInputs.forEach(function (i) {
+        if (i.value.trim() === '') {
+            i.classList.add('error');
+            setError = true;
+        } else {
+            i.classList.remove('error');
+        }
+    });
+    if (setError) {
+        e.preventDefault();
+    }
+});
